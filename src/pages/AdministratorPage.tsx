8637a29f3fcfbf6ecde203a2c4c7aee06b3cbf94
Tomek <tomasz.topor@sobre.pl>
2023-08-28 11:58:51 +0200

    Remove Background

--- src/pages/AdministratorPage.tsx
+++ src/pages/AdministratorPage.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Menu from "../components/Menu";
 import AdminMenu from "../components/AdminMenu";
-import Background from "../img/Green_bg2.webp";
 import styled from "styled-components";
-
-const BackgroundImage = styled.img`
-  position: absolute;
-  bottom: 0;
-`;
 
 const AdministratorPage = () => {
   return (
     <div>
       <Menu />
       <AdminMenu />
-      <BackgroundImage src={Background} />
     </div>
   );
 };
